2b5d383ad815e2a625b9116722fc1d2feb282a41
Thiago Miyazaki <thiagomi@motorola.com>
2024-10-22 23:08:00 -0300

    correctlying push message to uart

--- main.s
+++ main.s
@@ -35,23 +35,28 @@ while True {
 _start:
     movia r4, ASK_COMMAND_STRING
     call write_string
+    END:
+        br END
 
 
 write_string:
-    /* argumentos:
+    /* 
+    argumentos:
         r4 : endereco inicial de leitura (primeiro caractere)
     */
+    movia   r10, UART_CONTROL_REG
+    movia   r12, UART_DATA_REG
     POLLING_ESCRITA:
-        ldwio  r8, 0(r10)                   /* copia o valor de CONTROL_REG para r13 */
+        ldwio  r8, 0(r10)                   /* copia o valor de CONTROL_REG */
         andhi  r9, r13, 0xFFFF              /* aplica a mascara para verificar WSPACE */
-        beq    r9, r0, POLLING_ESCRITA      /* se n houver espaco no buffer, volte */
+        bne    r9, r0, POLLING_ESCRITA      /* se n houver espaco no buffer, volte */
 
-        ldb    r10, 0(r4)                   /* carrega chars */
-        beq    r10, 0, RET_STR              /* se achou o zero entao acabou a string */
-        stwio  dado, 0(r9)                  /* escreve o dado em DATA de DATA_REG */
+        ldb    r11, 0(r4)                    /* carrega chars */
+        beq    r11, r0, RET_STR              /* se achou o zero entao acabou a string */
+        stwio  r11, 0(r12)                   /* escreve o dado em DATA de DATA_REG */
         addi   r4, r4, 1
 
-        br POLLING_LEITURA
+        br POLLING_ESCRITA
     
     RET_STR:
         ret
